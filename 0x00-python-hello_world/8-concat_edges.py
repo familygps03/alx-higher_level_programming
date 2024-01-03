97e790c82cbedb11150701f5636b1939b4fff990
familygps03 <gmozey02@gmail.com>
2024-01-03 23:48:05 +0000

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+original_str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+modified_str = original_str[38:66] + ' ' + original_str[-22:-18] + ' ' + original_str[:6]
+print(modified_str)
