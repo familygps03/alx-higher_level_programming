25a9c0a87cbd77de4d2c894e6a2c94eab988c331
familygps03 <gmozey02@gmail.com>
2024-01-04 00:43:35 +0000

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- * check_cycle - Checks if a linked list has a loop.
- * @list: Pointer to the head of the linked list.
+ * check_cycle - checks if a linked list has a loop
+ * @list: this is pointer to the head of the linked list
  *
- * Return: 1 if the linked list has a loop, 0 otherwise.
+ * Return: 1: if linked list have a loop
+ *	0: if not
  **/
 
 int check_cycle(listint_t *list)
@@ -15,10 +16,12 @@ int check_cycle(listint_t *list)
 
 	if (list == NULL)
 		return (0);
+
 	while (slow_ptr && fast_ptr && fast_ptr->next)
 	{
 		slow_ptr = slow_ptr->next;
 		fast_ptr = fast_ptr->next->next;
+
 		if (slow_ptr == fast_ptr)
 		{
 			return (1);
