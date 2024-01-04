31bedfc4f3069618b2ad1d7dc186c6fce3505a99
familygps03 <gmozey02@gmail.com>
2024-01-04 02:56:44 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    char = chr(i)
-	if i % 2 == 0:
-	else:
-	    chr(i - 32)
-        print("{:s}".format(char), end='')
+    if i % 2 == 0:
+       char = chr(i)
+    else:
+	chr(i - 32)
+    print("{:s}".format(char), end='')
