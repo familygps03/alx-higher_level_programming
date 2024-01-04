b8bd999c9d697592b7dd5e3f28065001bd8015a4
familygps03 <gmozey02@gmail.com>
2024-01-04 23:22:33 +0000

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char_code in range(ord('a'), ord('z') + 1):
-    if char_code not in (ord('e'), ord('q')):
-       print("{:chr}".format(char_code), end='')
+for char_code in range(97, 123):
+    if char_code != 101 and char_code != 113:
+       print("{}".format(chr(char_code)), end="")
