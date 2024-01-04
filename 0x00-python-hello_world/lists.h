a65ff4d5b5b72921f63c4ff2bbeee287a536d73e
familygps03 <gmozey02@gmail.com>
2024-01-04 01:07:58 +0000

    10-check_cycle.c

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list in python
  * @n: integer in python
@@ -18,6 +21,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-#include <stdlib.h>
-#endif
+
+#endif /* LISTS_H */
+
 
