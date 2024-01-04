ca44828675219f7c86a810c8653924dd4bd180c2
familygps03 <gmozey02@gmail.com>
2024-01-04 22:42:56 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 n = abs(number) % 10
+if number < 0:
+    n = number % -10
+else:
+    n = number % 10
+
 if n > 5:
     phrase = "greater than 5"
 elif n == 0:
     phrase = "0"
 else:
     phrase = "less than 6 and not 0"
-print("Last digit of {} is {} and is {}".format(number, n, phrase))
+print(f"Last digit of {number} is {n} and is {phrase}")
