1ec0cca24adb4c5ba460449ae8c05e379c46776c
familygps03 <gmozey02@gmail.com>
2024-01-04 23:57:18 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for char_code in range(0, 100):
-    print("{:02d}{}".format(char_code, ", " if char-code <= 98 else "\n"), end=''))
+    print("{:02d}".format(char_code), end='\n' if char_code == 99 else ", ")
