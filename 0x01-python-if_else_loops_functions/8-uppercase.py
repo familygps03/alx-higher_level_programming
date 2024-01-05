9b5693ae6abc9c976de995f203a55440eb5e6a23
familygps03 <gmozey02@gmail.com>
2024-01-05 00:33:01 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def convert_to_uppercase(char):
-    return chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+    return chr(ord(char) - 32) 
+	if 'a' <= char <= 'z' 
+	else char
 def uppercase(input_str):
     result_str = ""
     for character in input_str:
