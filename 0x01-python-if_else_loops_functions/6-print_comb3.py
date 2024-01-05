e34f6ab5a527669f9b93a4cc7b1f2cbb8d65d873
familygps03 <gmozey02@gmail.com>
2024-01-05 00:06:25 +0000

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for char_code in range(0, 90):
+    if char_code % 10 != char_code / 10 and char_code % 10 > char_code / 10:
+        print("{:02d}{}".format(char_code, ", " if char_code < 89 else '\n'), end='')
