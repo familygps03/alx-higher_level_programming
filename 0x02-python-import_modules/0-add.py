17f1ede08ccedfcc4866a3d332b01d26c8f71130
familygps03 <gmozey02@gmail.com>
2024-01-06 01:55:11 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
     i = 1
     j = 2
-    print("{:d} + {:d} = {:d}".format(i, j, add(i, j)))
+    result = add(i, j)
+    print("{} + {} = {}".format(a, b, result))
