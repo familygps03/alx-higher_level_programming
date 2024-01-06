8174425693010aac5b959cca1409cd0a257ccf88
familygps03 <gmozey02@gmail.com>
2024-01-06 01:06:55 +0000

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
-        result = add(a, b)
-        for i in range(4, 6):
-            result = add(result, i)
-        return result
+       result = add(a, b)
+       for j in range(4, 6):
+           result = add(result, j)
+       return result
     else:
         return sub(a, b)
