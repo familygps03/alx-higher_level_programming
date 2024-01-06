833804479e0ef839c5da266faa44091db43d8967
familygps03 <gmozey02@gmail.com>
2024-01-06 02:12:51 +0000

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0")
+    else:
+        result = sum(int(arg) for arg in argv[1:])
+        print(result)
