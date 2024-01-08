41a42db016563d2c38c390981be0d96cacf37312
familygps03 <gmozey02@gmail.com>
2024-01-08 23:33:57 +0000

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,9 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0
         return my_list
-    elif idx >= len(my_list):
-	    return my_list
+    list_len = len(my_list)
+    elif idx >= lenlist_len - 1:
+	  return my_list
     else:
         my_list[idx] = element
         return my_list
