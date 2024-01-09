2524b7704a5375578966e79b91fc87a7d45ace62
familygps03 <gmozey02@gmail.com>
2024-01-09 23:10:09 +0000

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not my_list:
-        return my_list
     if idx < 0 or idx >= len(my_list):
         return my_list
-    del my_list[idx]
-    print("Modified list:", my_list)
+    my_list.pop(idx)
     return my_list
