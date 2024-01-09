08597e6bbfec584a959b1b0e1b549f384e2b16a9
familygps03 <gmozey02@gmail.com>
2024-01-09 23:36:23 +0000

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,76 +1,33 @@
+#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * Definition for singly-linked list.
- * struct listint_s {
- *     int n;
- *     struct listint_s *next;
- * };
- * typedef struct listint_s listint_t;
- */
-
-/**
- * reverse_list - Reverse a linked list.
- * @head: Pointer to the head of the list.
+ * is_palindrome - Checks if a list is a palindrome.
  *
- * This function reverses a linked list in-place.
+ * @head: Pointer to the first node.
  *
- * Return: Pointer to the new head of the reversed list.
- */
-listint_t *reverse_list(listint_t *head)
-{
-	listint_t *prev = NULL, *current = head, *next;
-
-	while (current != NULL)
-	{
-
-	next = current->next;
-
-	current->next = prev;
-
-	prev = current;
-
-	current = next;
-
-	}
-return (prev);
-}
-
-/**
- * is_palindrome - Check if a linked list is a palindrome.
- * @head: Pointer to the head of the list.
- * Return: 1 if palindrome, 0 otherwise.
+ * Return: 1 on success, 0 if fail.
  */
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-	{
-	return (1);
-	}
+	listint_t *tmp = *head;
+	int values[2048], i = 0, cLoop, limit;
 
-	listint_t *slow = *head, *fast = *head, *second_half, *prev_slow = *head;
-
-	while (fast != NULL && fast->next != NULL)
-	{
-	fast = fast->next->next;
-	prev_slow = slow;
-	slow = slow->next;
-	}
-	if (fast != NULL)
-	{
-	slow = slow->next;
-	}
-	second_half = reverse_list(slow);
-	while (second_half != NULL)
-	{
-		if ((*head)->n != second_half->n)
-		{
-		return (0);
-		}
-		*head = (*head)->next;
-		second_half = second_half->next;
-		}
-		prev_slow->next = reverse_list(prev_slow->next);
+	if (head == NULL || *head == NULL)
 		return (1);
+
+	while (tmp != NULL)
+	{
+		values[i] = tmp->n;
+		i++;
+		tmp = tmp->next;
+	}
+	limit = (i % 2 == 0) ? i / 2 : (i + 1) / 2;
+
+	for (cLoop = 0; cLoop < limit; cLoop++)
+		if (values[cLoop] != values[i - 1 - cLoop])
+			return (0);
+
+	return (1);
 }
