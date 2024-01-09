fe3c52ad5af11868957d1f79a5c6eff4728c8924
familygps03 <gmozey02@gmail.com>
2024-01-09 23:06:36 +0000

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return []
-    result = [num % 2 == 0 for num in my_list]
-    print("Result:", result)
-    return result
+    result_list = [True if i % 2 == 0 else False for i in my_list]
+    return result_list
