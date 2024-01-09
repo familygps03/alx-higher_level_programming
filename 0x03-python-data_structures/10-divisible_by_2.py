10e8d8e2f19ed5131c02d48eb57ba0dd12bbac7c
familygps03 <gmozey02@gmail.com>
2024-01-09 01:17:57 +0000

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return []
+    result = [num % 2 == 0 for num in my_list]
+    print("Result:", result)
+    return result
