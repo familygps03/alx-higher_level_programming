cca959cec3e6bac2797316690f7e456c20be1c37
familygps03 <gmozey02@gmail.com>
2024-01-11 22:43:40 +0000

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,51 +3,52 @@
 #include <string.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - This is the fuction that Generates and
+ * prints passwords for the crackme5 executable.
+ * @argc: This is the number of arguments supplied to the program.
+ * @argv: this is An array of pointers to the arguments.
  *
- * Return: Always 0.
+ * Return: Always zero.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
-	int username_length = strlen(argv[1]), username_sum = 0, max_char = 0;
+	int len = strlen(argv[1]), a, temp;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	password[0] = codex[(username_length ^ 59) & 63];
+	temp = (len ^ 59) & 63;
+	password[0] = codex[temp];
 
-	for (int index = 0; index < username_length; index++)
-		username_sum += argv[1][index];
-	password[1] = codex[(username_sum ^ 79) & 63];
+	temp = 0;
+	for (a = 0; a < len; a++)
+		temp += argv[1][a];
+	password[1] = codex[(temp ^ 79) & 63];
 
-	int username_product = 1;
+	temp = 1;
+	for (a = 0; a < len; a++)
+		temp *= argv[1][a];
+	password[2] = codex[(temp ^ 85) & 63];
 
-	for (int index = 0; index < username_length; index++)
-		username_product *= argv[1][index];
-	password[2] = codex[(username_product ^ 85) & 63];
-
-	for (int index = 0; index < username_length; index++)
+	temp = 0;
+	for (a = 0; a < len; a++)
 	{
-		if (argv[1][index] > max_char)
-			max_char = argv[1][index];
+		if (argv[1][a] > temp)
+			temp = argv[1][a];
 	}
-	srand(max_char ^ 14);
+	srand(temp ^ 14);
 	password[3] = codex[rand() & 63];
 
-	int squared_sum = 0;
+	temp = 0;
+	for (a = 0; a < len; a++)
+		temp += (argv[1][a] * argv[1][a]);
+	password[4] = codex[(temp ^ 239) & 63];
 
-	for (int index = 0; index < username_length; index++)
-		squared_sum += (argv[1][index] * argv[1][index]);
-	password[4] = codex[(squared_sum ^ 239) & 63];
-
-	for (int index = 0; index < argv[1][0]; index++)
-		max_char = rand();
-	password[5] = codex[(max_char ^ 229) & 63];
+	for (a = 0; a < argv[1][0]; a++)
+		temp = rand();
+	password[5] = codex[(temp ^ 229) & 63];
 
 	password[6] = '\0';
 	printf("%s", password);
 	return (0);
-
 }
