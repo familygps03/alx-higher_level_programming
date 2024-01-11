372aa6bb9aecddeaf18dbb9461ed527da159372b
familygps03 <gmozey02@gmail.com>
2024-01-11 00:52:16 +0000

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    while value in a_dictionary.values():
-        for i, j in a_dictionary.items():
-            if j == value:
-                del a_dictionary[i]
-                break
-    return (a_dictionary)
+def complex_delete(prmDictionary, prmValue):
+    for key, value in sorted(prmDictionary.items()):
+        if value == prmValue:
+            del prmDictionary[key]
+    return prmDictionary
