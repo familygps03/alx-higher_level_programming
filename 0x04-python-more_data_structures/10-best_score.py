b1d581992d6077f1058991d6873a64eb07d8ceca
familygps03 <gmozey02@gmail.com>
2024-01-11 03:09:01 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-def find_best_score(scores):
-    if not scores or len(scores) == 0:
+def find_best_score(a_dict):
+    if not isinstance(a_dict, dict) or len(a_dict) == 0:
         return None
-    return max(scores, key=scores.get)
+
+    result_key = list(a_dict.keys())[0]
+    best_score = list(a_dict.values())[0]
+    
+    for key, value in a_dict.items():
+        if value > best_score:
+            best_score = value
+            result_key = key
+            
+    return result_key
