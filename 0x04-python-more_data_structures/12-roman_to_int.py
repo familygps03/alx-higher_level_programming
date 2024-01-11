0e8812553dd946cf5915389fffc3cff1a8385c93
familygps03 <gmozey02@gmail.com>
2024-01-11 02:54:24 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
+    if (not isinstance(roman_string, str) or roman_string is None):
+        return (0)
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
+    roman_d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = 0
 
-    for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            result += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+    for m in range(len(roman_string)):
+        if roman_d.get(roman_string[m], 0) == 0:
+            return (0)
+
+        if (m != (len(roman_string) - 1) and
+                roman_d[roman_string[m]] < roman_d[roman_string[m + 1]]):
+            number += roman_d[roman_string[m]] * -1
+
         else:
-            result += roman_dict[roman_string[i]]
-
-    return result
-
-# Example usage:
-print(roman_to_int("III"))  # Output: 3
-print(roman_to_int("IX"))   # Output: 9
-print(roman_to_int("LVIII")) # Output: 58
-print(roman_to_int("MCMXCIV"))  # Output: 1994
-print(roman_to_int(None))  # Output: 0
-print(roman_to_int(123))   # Output: 0
+            number += roman_d[roman_string[m]]
+    return (number)
