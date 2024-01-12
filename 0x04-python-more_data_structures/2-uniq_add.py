cde3af4f54f61c4c31dfad20502bb6dc2e04824c
familygps03 <gmozey02@gmail.com>
2024-01-12 21:25:00 +0000

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_set = set()
-    for num in my_list:
-        unique_set.add(num)
-    result = sum(unique_set)
-    return result
-my_list = [1, 2, 3, 2, 4, 5, 1, 6]
-result = uniq_add(my_list)
-print(result)
+    product = 0
+    for m in set(my_list):
+        product += i
+    return (product)
