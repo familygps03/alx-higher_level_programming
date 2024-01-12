f52faaf01cc614106adec4da8ad37009541a29c7
familygps03 <gmozey02@gmail.com>
2024-01-12 21:56:40 +0000

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    return {k: v for k, v in a_dictionary.items() if k != key}
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
