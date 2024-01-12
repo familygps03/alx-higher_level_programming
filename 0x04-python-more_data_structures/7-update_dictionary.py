caf919c0f24cbd14ef97f25638a293338f05c937
familygps03 <gmozey02@gmail.com>
2024-01-12 21:45:45 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-
-update_dictionary(my_dict, 'b', 5)
-update_dictionary(my_dict, 'd', 4)
-print(my_dict)
+    new_dict = a_dictionary.copy()
+    new_dict[key] = value
+    return new_dict
