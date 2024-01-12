68db2abe1a865397a4a8e3165a8ea766a3a66f37
familygps03 <gmozey02@gmail.com>
2024-01-12 22:04:13 +0000

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    multiplied_dict = {key: value * 2 for key, value in a_dictionary.items()}
-    return multiplied_dict
 
-my_dict = {'a': 1, 'b': 2, 'c': 3}
+def multiply_values_by_2(input_dictionary):
+    modified_dictionary = input_dictionary.copy()
 
-result_dict = multiply_by_2(my_dict)
+    for key, value in list(modified_dictionary.items()):
+        modified_dictionary[key] = value * 2
 
-print("Original Dictionary:", my_dict)
-print("Multiplied by 2 Dictionary:", result_dict)
+    return modified_dictionary
