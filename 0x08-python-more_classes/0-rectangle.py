54300fde684108e22ce1144b570aa06119e2368d
familygps03 <gmozey02@gmail.com>
2024-01-29 20:26:12 +0000

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an empty Rectangle class"""
+
+
+class Rectangle:
+    """A class representing the properties of a rectangle.
+    This class is currently empty.
+    """
+    pass
