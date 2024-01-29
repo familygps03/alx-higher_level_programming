843dedcdcdad8906d995f7181de59fecaa02a56b
familygps03 <gmozey02@gmail.com>
2024-01-29 19:36:55 +0000

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,58 +1,54 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 
-"""
-Module soln for The N Queens Puzzle
-"""
+class NQueensSolver:
+    def __init__(self, n):
+        self.n = n
+        self.board = [-1] * n
 
-class Queen:
-    """
-    class Queen to solve nQueens problem
-    """
-    def can_move(self, x, y, right):
-        """
-        Checking if queen can move in the valid constraint column given
-        """
-        for a in range(x):
-            if right[a] == y:
-                return (False)
-            if abs(right[a] - y) == (x - a):
-                return (False)
-            return (True)
+    def is_safe(self, row, col):
+        for i in range(row):
+            if (
+                self.board[i] == col
+                or self.board[i] - i == col - row
+                or self.board[i] + i == col + row
+            ):
+                return False
+        return True
 
-    def soln(self, n, N, right):
-        """
-        Finding all the right combos that can happen using recursion
-        """
-        if n == N:
-            print("[", end="")
-            for j in range(N):
-                print("{}, {}]".format(j, right[j]), end="")
-                if j < N - 1:
-                    print(", ", end="")
-            print("]")
+    def solve_queens(self, row):
+        if row == self.n:
+            self.print_solution()
             return
 
-        for j in range(N):
-            if self.can_move(n, j, right):
-                right[n] = j
-                self.solution(n + 1, N, right)
+        for col in range(self.n):
+            if self.is_safe(row, col):
+                self.board[row] = col
+                self.solve_queens(row + 1)
 
-if __name__ == "__main__":
-    count = len(argv)
+    def print_solution(self):
+        print("[", end="")
+        for i in range(self.n):
+            print("{}, ".format(self.board[i]), end="")
+        print("\b\b]")
 
-    if count != 2:
+def main():
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    else:
-        try:
-            N = int(arg[1])
-        except:
-            print("N must be a number")
-            exit(1)
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
     if N < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    final = Queen()
-    final.soln(0, N, [None for i in range(N)])
+    solver = NQueensSolver(N)
+    solver.solve_queens(0)
+
+if __name__ == "__main__":
+    main()
