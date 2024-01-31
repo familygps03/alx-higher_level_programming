0d781add6f5ffa43e0b835d1bb4db0bc1f3e7590
familygps03 <gmozey02@gmail.com>
2024-01-31 23:33:15 +0000

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-"""Defines a function add_integer(a, b=98) that adds two integers.
-
-Attributes:
-    add_integer: function that adds two integers.
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """Adds two integer and/or float values.
+    """Return the integer addition of a and b.
 
-    Args:
-        a (int): First value
-        b (int, optional): Second value. Defaults to 98.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: If a and b are not integers or floats.
-
-    Returns:
-        int: Sum of a and b.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("The first value must be an integer or float")
-
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("The second value must be an integer or float")
-
-    # Convert float values to integers
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
-
-    return a + b
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
