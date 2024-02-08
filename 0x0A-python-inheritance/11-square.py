8f81387b9a4dc5ef8a4d3a578e1d85b6ec9dbbf6
familygps03 <gmozey02@gmail.com>
2024-02-08 00:06:56 +0000

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
-"""Defines a class Square based on Rectangle."""
+"""Defines a class Square based on 9-rectangle.py.
 
-from typing import Union
+Attributes:
+    width (int): width of the rectangle.
+    height (int): height of the rectangle.
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a class Square as a subclass of Rectangle."""
+    """Defines a class Square.
 
-    def __init__(self, size: int):
-        """Initializes a Square instance.
+    Args:
+        Rectangle (Rectangle): rectangle
+    """
+
+    def __init__(self, size):
+        """Creates new instances of class Square.
 
         Args:
-            size (int): The size of one side of the square.
+            size (int): size of 1 side of square.
         """
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
-    def __str__(self) -> str:
+    def __str__(self):
         """Returns a string representation of the square.
 
         Returns:
-            str: A string indicating the square's size.
+            str: square.
         """
-        return f"[Square] {self.width}/{self.height}"
+        return ("[Square] {}/{}".format(self.__size, self.__size))
 
-    def area(self) -> Union[int, float]:
-        """Computes the area of the square.
+    def area(self):
+        """Calculates the area of a square.
 
         Returns:
-            Union[int, float]: The area of the square.
+            int: the area of the square.
         """
-        return self.width * self.height
+        return self.__size ** 2
