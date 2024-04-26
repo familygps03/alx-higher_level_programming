96807ab0eea53e8c43ff6f8f92a7b46f07e7facf
familygps03 <gmozey02@gmail.com>
2024-04-26 02:19:32 +0200

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
+# Script that sends a POST request and displays the body response
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
